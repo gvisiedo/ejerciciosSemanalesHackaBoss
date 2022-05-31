a1ec5df724f9df12150d7f25b57e40dcf259d6b6
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2022-05-31 20:23:47 +0200

    ejercicios semana 5 js empezados

---
+++ semana-05-javascriptII/1-Objeto/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const people = {
+  Maria: 20,
+  Ana: 14,
+  Luis: 16,
+  Pepe: 35,
+  Manuel: 50,
+  Teresa: 12,
+  Daniel: 27,
+  Irene: 23,
+  Alex: 10,
+};
+
+const mayorEdad = 18;
+
+if (people.Maria >= mayorEdad){
+  console.log(`Maria es mayor de edad`);
+}else{
+  console.log(`Maria es menor de edad`);
+};
+if (people.Ana >= mayorEdad){
+  console.log(`Ana es mayor de edad`);
+}else{
+  console.log(`Ana es menor de edad`);
+};
+if (people.Luis >= mayorEdad){
+  console.log(`Luis es mayor de edad`);
+}else{
+  console.log(`Luis es menor de edad`);
+};
+if (people.Pepe >= mayorEdad){
+  console.log(`Pepe es mayor de edad`);
+}else{
+  console.log(`Pepe es menor de edad`);
+};
+if (people.Manuel >= mayorEdad){
+  console.log(`Manuel es mayor de edad`);
+}else{
+  console.log(`Manuel es menor de edad`);
+};
+if (people.Teresa >= mayorEdad){
+  console.log(`Teresa es mayor de edad`);
+}else{
+  console.log(`Teresa es menor de edad`);
+};
+if (people.Daniel >= mayorEdad){
+  console.log(`Daniel es mayor de edad`);
+}else{
+  console.log(`Daniel es menor de edad`);
+};
+if (people.Irene >= mayorEdad){
+  console.log(`Irene es mayor de edad`);
+}else{
+  console.log(`Irene es menor de edad`);
+};
+if (people.Alex >= mayorEdad){
+  console.log(`Alex es mayor de edad`);
+}else{
+  console.log(`Alex es menor de edad`);
+};
+
